e703061d5b4b29c6db07f18a67f6e3162acd08f7
mpriyankdev <mpriyank.dev@gmail.com>
2019-12-19 20:58:25 +0530

    optimizing imports

--- monitoring-config-svc/src/main/java/com/monitor/monitoringconfigsvc/MonitoringConfigSvcApplication.java
+++ monitoring-config-svc/src/main/java/com/monitor/monitoringconfigsvc/MonitoringConfigSvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonitoringConfigSvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitoringConfigSvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringConfigSvcApplication.class, args);
+    }
 
 }
--- monitoring-svc/src/main/java/com/monitor/monitoringsvc/repository/ServicePropertiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monitor.monitoringsvc.repository;
-
-import com.monitor.monitoringsvc.entity.ServicePropertiesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServicePropertiesRepository extends JpaRepository<ServicePropertiesEntity, String> {
-}
