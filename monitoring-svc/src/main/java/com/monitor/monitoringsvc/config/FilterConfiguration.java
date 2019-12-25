d7db34bd092926bd2c1097204912de01a17dfb76
mpriyankdev <mpriyank.dev@gmail.com>
2019-12-25 13:57:51 +0530

    added swagger config and error-filter to map to a default service

--- monitoring-svc/src/main/java/com/monitor/monitoringsvc/config/FilterConfiguration.java
+++ monitoring-svc/src/main/java/com/monitor/monitoringsvc/config/FilterConfiguration.java
@@ -1,5 +1,6 @@
 package com.monitor.monitoringsvc.config;
 
+import com.monitor.monitoringsvc.filter.ErrorFilter;
 import com.monitor.monitoringsvc.filter.ResponsePostFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,10 @@ public class FilterConfiguration {
     @Bean
     public ResponsePostFilter responsePostFilter() {
         return new ResponsePostFilter();
+    }
+
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
     }
 }
